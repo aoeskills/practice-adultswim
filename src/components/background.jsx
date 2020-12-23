3eb843337f4dc9f50c51cc0d0b863753c43042e7
DESKTOP-D7SLJUM\ㄖㄖㄖ <a288252525566@gmail.com>
2020-12-23 17:08:20 +0800

    chore: 調整background component的顏色
    增加video filter讓影片跟背景顏色相同時依然可以看清logo

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -10,6 +10,7 @@ const Background = () => (
       <div id={styles.backgroundVideo}>
         <iframe title="background-video" src={`https://www.youtube.com/embed/${videoIDs[videoWeighting[Math.floor(Math.random() * 10) % 6]]}?autoplay=1&mute=1&start=${Math.floor(Math.random() * 180 + 20)}&loop=1`} frameBorder="0" allow="" />
       </div>
+      <div id={styles.videoFilter} />
       <div id={styles.cover}>
         <LogoCover id={styles.svg} />
       </div>
