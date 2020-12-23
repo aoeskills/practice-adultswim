6c0e7ad2f0b913277b1facc613d97be6bb4b1e23
DESKTOP-D7SLJUM\ㄖㄖㄖ <a288252525566@gmail.com>
2020-12-24 02:09:51 +0800

    chore: 將header中的logo套用interactiveButton

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Logo from '@/images/logo-fullname.svg';
 import styles from './header.scss';
+import InteractiveButton from './interactiveButton';
 
 const Header = () => (
   <div id={styles.header}>
-    <a href="##">
-      <Logo id={styles.logo} />
-    </a>
+    <InteractiveButton>
+      <a href="##">
+        <Logo id={styles.logo} />
+      </a>
+    </InteractiveButton>
     <div className={styles.descripion}>
       Late night show
     </div>
