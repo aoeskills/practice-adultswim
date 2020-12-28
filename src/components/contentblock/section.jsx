4135985174a3d21f5d9af32c94a0e516668f56cf
DESKTOP-D7SLJUM\ㄖㄖㄖ <a288252525566@gmail.com>
2020-12-29 00:48:49 +0800

    chore: 替section component增加參數

--- src/components/contentblock/section.jsx
+++ src/components/contentblock/section.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './fullPage.scss';
+import styles from './section.scss';
 
 /**
  * 佔整個螢幕高與寬的layout。
  * 提供 alignH 設定水平對齊位置，有center與left兩個選項
  */
-const FullPage = ({ children, alignH }) => (
-  <div className={`${styles['full-page']} ${styles[alignH]}`}>
+const Section = ({
+  children, size, alignH, alignV,
+}) => (
+  <div className={`${styles['full-page']} ${styles[size]} ${styles[`v-${alignV}`]} ${styles[`h-${alignH}`]}`}>
     {children}
   </div>
 );
 
-FullPage.propTypes = {
+Section.propTypes = {
   children: PropTypes.node,
+  size: PropTypes.string,
   alignH: PropTypes.string,
+  alignV: PropTypes.string,
 };
-FullPage.defaultProps = {
+Section.defaultProps = {
   children: '',
+  size: 'full',
   alignH: 'center',
+  alignV: 'center',
 };
 
-export default FullPage;
+export default Section;
