8dbb38c82d91976c8e06aab3522bc8634624313c
DESKTOP-D7SLJUM\ㄖㄖㄖ <a288252525566@gmail.com>
2020-12-30 03:15:58 +0800

    chore: 替 H1 元件增加 diff參數
    改善color對比度。
    如果diff設置為false，則不要使用blend-mode。

--- src/components/contentblock/h1.jsx
+++ src/components/contentblock/h1.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './h1.scss';
 
-const H1 = ({ children }) => (
-  <h1 className={styles.header}>
+const H1 = ({ children, diff, style }) => (
+  <h1 className={`${styles.header} ${diff ? styles.diff : ''}`} style={style}>
     {children}
   </h1>
 );
 
 H1.propTypes = {
   children: PropTypes.node,
+  style: PropTypes.object,
+  diff: PropTypes.bool,
 };
 H1.defaultProps = {
   children: '',
+  style: {},
+  diff: true,
 };
 
 export default H1;
