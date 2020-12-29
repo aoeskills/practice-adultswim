225e67a18448ed8faccbfc29101d6af36db981dc
DESKTOP-D7SLJUM\ㄖㄖㄖ <a288252525566@gmail.com>
2020-12-30 03:16:49 +0800

    chore: 替 section 元件增加 title 參數
    如果有輸入title參數則會出現section的title

--- src/components/contentblock/section.jsx
+++ src/components/contentblock/section.jsx
@@ -7,9 +7,12 @@ import styles from './section.scss';
  * 提供 alignH 設定水平對齊位置，有center與left兩個選項
  */
 const Section = ({
-  children, size, alignH, alignV,
+  children, size, alignH, alignV, title,
 }) => (
-  <div className={`${styles['full-page']} ${styles[size]} ${styles[`v-${alignV}`]} ${styles[`h-${alignH}`]}`}>
+  <div
+    className={`${styles['full-page']} ${styles[size]} ${styles[`v-${alignV}`]} ${styles[`h-${alignH}`]}`}
+  >
+    {title !== '' ? <div className={styles.titlebar}>{title}</div> : null}
     {children}
   </div>
 );
@@ -19,12 +22,14 @@ Section.propTypes = {
   size: PropTypes.string,
   alignH: PropTypes.string,
   alignV: PropTypes.string,
+  title: PropTypes.string,
 };
 Section.defaultProps = {
   children: '',
   size: 'full',
   alignH: 'center',
   alignV: 'center',
+  title: '',
 };
 
 export default Section;
