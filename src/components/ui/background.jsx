35a3d3f77a5d3b802e2bf0db4e99c6cde0450316
DESKTOP-D7SLJUM\ㄖㄖㄖ <a288252525566@gmail.com>
2020-12-31 13:02:38 +0800

    chore: 調整背景影片機率

--- src/components/ui/background.jsx
+++ src/components/ui/background.jsx
@@ -5,11 +5,11 @@ import { connect } from 'react-redux';
 import styles from './background.scss';
 //    <iframe title="backgroundVideo" width="1280" height="720" src="https://www.youtube.com/embed/wycjnCCgUes" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
 const videoIDs = ['pCpLWbHVNhk', 'u1pwtSBTnPU', 'FX0GzeY3BDM'];
-const videoWeighting = [0, 0, 0, 1, 1, 2];
+const videoWeighting = [0, 0, 0, 1, 1];
 const Video = React.memo(() => (
   <iframe
     title="background-video"
-    src={`https://www.youtube.com/embed/${videoIDs[videoWeighting[Math.floor(Math.random() * 10) % 6]]}?autoplay=1&mute=1&start=${Math.floor(Math.random() * 180 + 20)}&loop=1`}
+    src={`https://www.youtube.com/embed/${videoIDs[videoWeighting[Math.floor(Math.random() * 10) % 5]]}?autoplay=1&mute=1&start=${Math.floor(Math.random() * 180 + 20)}&loop=1`}
     frameBorder="0"
     allow=""
   />
